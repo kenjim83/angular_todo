d2c2f64b60810d05c03183735d505215086cf19c
Kenji Miwa <kenjim83@gmail.com>
2014-09-03 15:32:30 -0700

    playing with ui-views

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 //
-todo = angular.module('Todo', [])
+todo = angular.module('Todo', ['ngStorage', 'ui.router'])
 
-todo.controller('Tasks', function($scope){
+todo.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/') // if no other routes found, use this.
+
+  $stateProvider.state('index', {
+    url: "/",
+    templateUrl: "tasks.html",
+    controller: "Tasks"
+  })
+
+  $stateProvider.state('view', {
+    url: '/:id',
+    template: '<h1>Hi</h1>'
+
+  })
+
+})
+
+
+todo.controller('Tasks', function($scope, $localStorage){
+
+  // Research "State Resolves" / "UI Router Resolves"
+
+  // Study Services.  Creating your own services
+
+  // Creating your own directives.  Stay away from learning directives for now. Use ONLY if needed.
+
   $scope.tasks=[{}];
+  if($localStorage.tasks){
+    $scope.tasks = $localStorage.tasks;
+  } else {
+    $scope.reset()
+  }
+
+  $scope.reset = function(){
+    $scope.tasks = $localStorage.tasks = [{}]
+  }
 
   $scope.add = function(){
     $scope.tasks.push({})
@@ -13,6 +47,6 @@ todo.controller('Tasks', function($scope){
   }
 
   $scope.tasks_template = 'tasks.html';
-})
 
+})
 
