e19d12b03003375970f2e525ac8bce655da84198
Kenji Miwa <kenjim83@gmail.com>
2014-09-03 14:19:30 -0700

    full CRUDiness

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-var toDo = angular.module('todo', []);
+//
+todo = angular.module('Todo', [])
 
-toDo.control
+todo.controller('Tasks', function($scope){
+  $scope.tasks=[{}];
+
+  $scope.add = function(){
+    $scope.tasks.push({})
+  }
+
+  $scope.remove = function(index){
+    $scope.tasks.splice(index, 1)
+  }
+
+  $scope.tasks_template = 'tasks.html';
+})
+
+
